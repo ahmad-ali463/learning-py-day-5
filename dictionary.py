5a2b39ba89140719bc2107f365c3b5e7bc8bd04f
Ahmad Ali <spyali463@gmail.com>
2024-12-08 14:08:19 +0500

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,53 @@
+# dict = {
+      
+#        "name" :  "Ahmad Ali",
+#        'marks' :  94.5,
+#        "subject" : ["java","C","python"],
+#        "age" : 19,
+
+   
+# }
+# dict["name"] = "Minha"
+# dict["surname"] = "junaid"
+# # print(dict["name"]),
+# # print(dict["age"]),
+# print(dict)
+
+# how to add nested if in dictionary
+
+# dict = {
+#     "name" : "Ali",
+#     "subject" :{
+#         "chem": 92,
+#         "phy" : 97,
+#         "eng" : 88,
+#         "urdu": 75
+#      }
+# }
+# print(dict["subject"]["eng"])
+
+
+# value is used for check the value
+# print(dict.values())
+
+# print(list(dict.values()))
+# .keys is used for check the length
+# print(len(dict.keys()))
+# .items is used for pairs
+# print((dict.items()))
+# print(list(dict.items()))
+
+# .get is used to pass the key and get the value
+
+# print(dict.get("subject"))
+# print(list(dict.get("subject")))
+
+
+# dict.update({"city" : "Faisalabad"})
+# print(dict)
+
+
+
+person_ages = {}
+person_ages["name"] = "Ahmad Ali"
+print(person_ages)
