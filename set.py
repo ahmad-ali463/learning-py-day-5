4b175414aed978b7a44481cfc1a6d82da8c649c1
Ahmad Ali <spyali463@gmail.com>
2024-12-08 15:20:18 +0500

    set

---
+++ set.py
@@ -0,0 +1,26 @@
+# set = {2 , 2 ,2,3 ,4 ,5 ,5 ,6}
+# print(type(set))
+
+# set.add(9)
+# print(set)
+# set.remove(2)
+# print(set)
+# set.clear()
+# print(set)
+
+
+
+# add = ["apna college", "Faisalabad", "Ahmad Ali", "Haider"]
+# print(add.pop())
+# print(add)
+
+# set1 = {1 ,3 ,5}
+# set2 = {2,4}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+
+list = { "python","java","fortan","C","python","java","C++",
+}
+print(type(list))
+print(list)
